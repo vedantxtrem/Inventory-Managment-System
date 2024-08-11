5fc9a92ad7455fab2dff6450248d5c3fcdc3c8ee
vedant <vedant@ssipmt.com>
2024-08-12 00:28:57 +0530

    link

--- Client/src/Helper/axioxInstance.helper.js
+++ Client/src/Helper/axioxInstance.helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for API requests
-const BASE_URL = "http://localhost:5050/api/v1";
+const BASE_URL = "https://ims-1urn.onrender.com/api/v1/";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
